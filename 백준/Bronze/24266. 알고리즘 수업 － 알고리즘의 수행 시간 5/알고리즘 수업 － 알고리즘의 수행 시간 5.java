cd87c04118c8eea26c0087842bddc4f4d9c8c765
Kim-HyeonSeong <124437675+Kim-HyeonSeong@users.noreply.github.com>
2024-01-04 16:47:50 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 124 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main{
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        Long n = Long.parseLong(st.nextToken());
+
+        System.out.println(n*n*n);
+        System.out.println(3);
+    }
+}
